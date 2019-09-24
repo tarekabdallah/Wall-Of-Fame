446208a88fc2bef04f90ce7bf8f8ccd268587bc3
tarekabdallah <tarekabd1995@gmail.com>
2019-09-24 22:51:59 +0300

    Refactored ViewController to TrendingViewController

---
+++ Wall Of Fame/View/TrendingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wall Of Fame
+//
+//  Created by Tarek Abdallah on 24/9/19.
+//  Copyright © 2019 Tarek. All rights reserved.
+//
+
+import UIKit
+
+class TrendingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
