535fd99276713a7a49609708d99eb9b0003b2df1
tarekabdallah <tarekabd1995@gmail.com>
2019-09-24 23:22:52 +0300

    Created TrendingViewController skeleton

--- Wall Of Fame/View/TrendingViewController.swift
+++ Wall Of Fame/View/TrendingViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class TrendingViewController: UIViewController {
 
+    @IBOutlet weak var tableView:UITableView!
+    
+    var trendingRepositories:[TrendingViewModel] = [TrendingViewModel]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension TrendingViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return trendingRepositories.count
+    }
+}
