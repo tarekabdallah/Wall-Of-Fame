2fef48774e051e6b52e279775336518a86cd345d
tarekabdallah <tarekabd1995@gmail.com>
2019-09-24 23:55:37 +0300

    created repositories alamofire requester

--- Wall Of Fame/ViewModel/TrendingViewModel.swift
+++ Wall Of Fame/ViewModel/TrendingViewModel.swift
@@ -7,10 +7,14 @@
 //
 
 import Foundation
+import Alamofire
 class TrendingViewModel{
-    private let gitRepositories:[GitRepositoryModel]
-    init(gitRepositories:[GitRepositoryModel]) {
-        self.gitRepositories = gitRepositories
+    private(set) var webService :WebService!
+    
+    private let gitRepositories:[GitRepositoryModel] = [GitRepositoryModel]()
+    var page:Int = 0
+    init(webService:WebService) {
+        self.webService = webService
     }
 }
 extension TrendingViewModel{
@@ -38,6 +42,29 @@ extension TrendingViewModel{
     func getRepoOwnerAccountURL(index:Int) -> URL{
         return URL(string: gitRepositories[index].owner.accountURL)!
     }
+}
 
 
+extension DataRequest {
+    fileprivate func decodableResponseSerializer<T: Decodable>() -> DataResponseSerializer<T> {
+        return DataResponseSerializer { _, response, data, error in
+            guard error == nil else { return .failure(error!) }
+            
+            guard let data = data else {
+                return .failure(AFError.responseSerializationFailed(reason: .inputDataNil))
+            }
+            
+            return Result { try JSONDecoder().decode(T.self, from: data) }
+        }
+    }
+    
+    @discardableResult
+    fileprivate func responseDecodable<T: Decodable>(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<T>) -> Void) -> Self {
+        return response(queue: queue, responseSerializer: decodableResponseSerializer(), completionHandler: completionHandler)
+    }
+    
+    @discardableResult
+    func responseRepositories(queue: DispatchQueue? = nil, completionHandler: @escaping (DataResponse<[GitRepositoryModel]>) -> Void) -> Self {
+        return responseDecodable(queue: queue, completionHandler: completionHandler)
+    }
 }
