e7c8ddd22ffab85b6ec55cd47c369c733ef9a732
tarekabdallah <tarekabd1995@gmail.com>
2019-09-30 22:35:35 +0300

    created git cell

---
+++ Wall Of Fame/View/Cell/GitRepositoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GitRepositoryTableViewCell.swift
+//  Wall Of Fame
+//
+//  Created by Tarek Abdallah on 30/9/19.
+//  Copyright © 2019 Tarek. All rights reserved.
+//
+
+import UIKit
+
+class GitRepositoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
