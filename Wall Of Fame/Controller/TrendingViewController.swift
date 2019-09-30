94953a1c7fc395e90427d62b6e11ec0885890bc7
tarekabdallah <tarekabd1995@gmail.com>
2019-09-30 22:35:08 +0300

    created and implemented trending view controller

--- Wall Of Fame/Controller/TrendingViewController.swift
+++ Wall Of Fame/Controller/TrendingViewController.swift
@@ -12,11 +12,15 @@ class TrendingViewController: UIViewController {
 
     @IBOutlet weak var tableView:UITableView!
     
-    var trendingRepositories:[TrendingViewModel] = [TrendingViewModel]()
+    var trendingViewModel:TrendingViewModel = TrendingViewModel(webService: WebService.shared)
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         // Do any additional setup after loading the view.
+        trendingViewModel.fetchTrendingRepositories { (success) in
+            self.tableView.reloadData()
+        }
     }
 
 
@@ -24,10 +28,11 @@ class TrendingViewController: UIViewController {
 
 extension TrendingViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = trendingViewModel.getRepoName(index: indexPath.row)
         return cell
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return trendingRepositories.count
+        return trendingViewModel.getTrendingRepoCount()
     }
 }
