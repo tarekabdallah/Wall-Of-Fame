95ba4b34d0ea644026bd953dd18b16354d0ac8fa
tarekabdallah <tarekabd1995@gmail.com>
2019-10-02 21:27:23 +0300

    updated fetching from viewModel and takingtable view

--- Wall Of Fame/Controller/TrendingViewController.swift
+++ Wall Of Fame/Controller/TrendingViewController.swift
@@ -16,24 +16,41 @@ class TrendingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.delegate = self
+        tableView.bounces = false
         tableView.register(UINib(nibName: "GitRepositoryTableViewCell", bundle: .main), forCellReuseIdentifier: "cell")
+        tableView.separatorStyle = .none
         // Do any additional setup after loading the view.
-        trendingViewModel.fetchTrendingRepositories { (success) in
-            self.tableView.reloadData()
-        }
+        
+        fetchTrendingRepositories()
     }
-
+    func fetchTrendingRepositories(){
+        trendingViewModel.fetchTrendingRepositories(tableView: self.tableView)
+    }
 
 }
 
-extension TrendingViewController:UITableViewDataSource{
+extension TrendingViewController:UITableViewDataSource,UITableViewDelegate{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.row == trendingViewModel.getTrendingRepoCount() - 1{
+            fetchTrendingRepositories()
+        }
+
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! GitRepositoryTableViewCell
         let repo = trendingViewModel.trendingGitRepositories[indexPath.row]
         cell.setup(repo: repo)
+        cell.selectionStyle = .none
         return cell
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return trendingViewModel.getTrendingRepoCount()
     }
+    func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+//        guard let currentPage = tableView.indexPathsForVisibleRows?.last else{return}
+//        if currentPage.row == trendingViewModel.getTrendingRepoCount() - 10{
+//            fetchTrendingRepositories()
+//        }
+
+    }
+    
 }
