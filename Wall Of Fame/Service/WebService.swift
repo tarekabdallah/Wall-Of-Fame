e5f641c8f673399def6b9cb560f722f659b7e3ce
tarekabdallah <tarekabd1995@gmail.com>
2019-10-07 00:21:16 +0300

    fetching Repos from 30 days or less

--- Wall Of Fame/Service/WebService.swift
+++ Wall Of Fame/Service/WebService.swift
@@ -13,8 +13,14 @@ class WebService {
     static let shared = WebService()
     func requestFetchGitRepositories(page:Int, success:@escaping (_ repositories:[GitRepositoryModel]) -> Void, failure:@escaping (_ message:String) -> Void){
         let url = "https://api.github.com/search/repositories"
-        
-        Alamofire.request(url, parameters: ["q":"created:>2017-10-22","sort":"stars","order":"desc","page":page], encoding: URLEncoding.queryString).responseArray(queue: DispatchQueue.main, keyPath: "items") { (response: DataResponse<[GitRepositoryModel]>) in
+        var dateComponent = DateComponents()
+        dateComponent.day = -30
+        let date = Calendar.current.date(byAdding: dateComponent, to: Date())!
+        let dateFormatter = DateFormatter()
+        dateFormatter.isLenient = true
+            dateFormatter.dateFormat  = "yyyy-MM-dd"
+
+        Alamofire.request(url, parameters: ["q":"created:>\(dateFormatter.string(from: date))","sort":"stars","order":"desc","page":page], encoding: URLEncoding.queryString).responseArray(queue: DispatchQueue.main, keyPath: "items") { (response: DataResponse<[GitRepositoryModel]>) in
             if let error = response.error{
                 let json = try? JSONSerialization.jsonObject(with: response.data ?? Data(), options: JSONSerialization.ReadingOptions.mutableLeaves) as? [String:Any]
                 failure(json?["message"] as? String ?? error.localizedDescription)
